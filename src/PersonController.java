37f3d9b2283be0c6ce6b665a2866883cf62d9c89
Noah Froberg <ndfroberg@linclemensvm3.csbsju.edu>
2016-03-06 20:55:53 -0600

    made class. empty methods

---
+++ src/PersonController.java
@@ -0,0 +1,23 @@
+
+public class PersonController {
+	
+	public boolean logOn(String username, String password, boolean steal){
+		//TODO	
+		return false;
+	}
+	
+	public boolean logOut(Person p){
+		//TODO	
+		return false;
+	}
+	
+	public boolean assignUI(Person p){
+		//TODO	
+		return false;
+	}
+	
+	public boolean assignPerson(PersonUI ui, Person p){
+		//TODO	
+		return false;
+	}
+}
