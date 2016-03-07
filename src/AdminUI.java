d5eecef1062b0818f74a3f2b8695668ff46a9b34
Noah Froberg <ndfroberg@linclemensvm3.csbsju.edu>
2016-03-06 20:36:42 -0600

    made class

--- src/AdminUI.java
+++ src/AdminUI.java
@@ -1,40 +1,42 @@
 
 public class AdminUI extends PersonUI {
 
+	public AdminController controller;
 	public AdminUI(Admin a) {
 		super.thisPerson = a;
+		this.controller = new AdminController(a);
 	}
 	public void addSchool(String name,String state,String location,String control,
 	int numStudents,double percentFemale,int SATVerb,double SATMath,double expenses,
 	double percentFinancialAid,int numberOfApplicants,double percentAdmitted,
 	double percentEnrolled,int academicsScale,int socialScale,int qualityOfLifeScale){
-		super.controller.addSchool(name,state,location,control,
+		this.controller.addSchool(name,state,location,control,
 				numStudents,percentFemale,SATVerb,SATMath,expenses,
 				percentFinancialAid,numberOfApplicants,percentAdmitted,
 				percentEnrolled,academicsScale,socialScale,qualityOfLifeScale);
 	}
 	public List<Person> getPeople(){
-		return super.controller.getPeople();	
+		return this.controller.getPeople();	
 	}
 	public List<Person> addPerson(String firstName, String lastName, String password, String username, String type){
-		return super.controller.addPerson(firstName,lastName,password,username,type);	
+		return this.controller.addPerson(firstName,lastName,password,username,type);	
 	}
 	public boolean changeStatus(Person p){
-		return super.controller.changeStatus(p);	
+		return this.controller.changeStatus(p);	
 	}
 	public boolean updatePerson(String firstName, String lastName, String password, String type){
-		return super.controller.addPerson(firstName,lastName,password,type);	
+		return this.controller.addPerson(firstName,lastName,password,type);	
 	}
 	public void updateSchool(School s, String name,String state,String location,String control,
 	int numStudents,double percentFemale,int SATVerb,double SATMath,double expenses,
 	double percentFinancialAid,int numberOfApplicants,double percentAdmitted,
 	double percentEnrolled,int academicsScale,int socialScale,int qualityOfLifeScale){
-		super.controller.updateSchool(s,name,state,location,control,
+		this.controller.updateSchool(s,name,state,location,control,
 				numStudents,percentFemale,SATVerb,SATMath,expenses,
 				percentFinancialAid,numberOfApplicants,percentAdmitted,
 				percentEnrolled,academicsScale,socialScale,qualityOfLifeScale);
 	}
 	public List<School> getSchools(){
-		return super.controller.getSchools(super.thisPerson);
+		return this.controller.getSchools(super.thisPerson);
 	}
 }
