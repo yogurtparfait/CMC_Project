d4b5b16e47d275f53edae59622290166b7d603cc
mmdaly <mmdaly@csbsju.edu>
2016-03-13 20:02:27 -0500

    added basic main contents to driver

--- src/TextDriver.java
+++ src/TextDriver.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 
 public class TextDriver {
 	
 	public static void main(String[] args) {
-		
+		int choice;
+		Scanner sc = new Scanner(System.in);
+		TextDriver driver = new TextDriver();
+
+		do 
+			{
+				System.out.println("Pick a use case: /n 1: login /n 3: manage profile /n 5: editProfile /n 0: Quit");
+				choice = sc.nextInt();
+				if (choice == 1)
+					driver.login();
+				else if(choice == 3)
+					driver.manageProfile();
+				else if(choice == 5)
+					driver.editProfile();
+				else
+					System.out.println("not valid try a different number");
+			}while( choice != 0);
 	}
-	
 	public void login(){}
 	public void manageProfile(){}
 	public void manageSchools(){}
