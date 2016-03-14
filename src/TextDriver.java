a66f4aad0db09a43b8cb46589fe7ee40c20f8409
mmdaly <mmdaly@csbsju.edu>
2016-03-13 19:51:25 -0500

    Created textDriver class with method headers for use cases

---
+++ src/TextDriver.java
@@ -0,0 +1,25 @@
+
+public class TextDriver {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void login(){}
+	public void manageProfile(){}
+	public void manageSchools(){}
+	public void searchSchools(){}
+	public void logout(){}
+	public void manageUsers(){}
+	public void editProfile(){}
+	public void removeSchool(){}
+	public void viewSchool(){}
+	public void viewSearchResults(){}
+	public void viewUser(){}
+	public void addUser(){}
+	public void addUniversity(){}
+	public void editUniversity(){}
+	public void viewSchoolAndRecommendations(){}
+	public void changeActiveStatusUser(){}
+	public void editUser(){}
+}
