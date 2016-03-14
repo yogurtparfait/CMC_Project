05dc06b2e993e65efbf330194099c176cb539378
mmdaly <mmdaly@csbsju.edu>
2016-03-13 20:42:10 -0500

    added login contents

--- src/TextDriver.java
+++ src/TextDriver.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
 public class TextDriver {
+	private Scanner sc; 
+	private PersonUI ui;
+	private UserUI userUI;
+	private AdminUI adminUI;
+	
+	
+	private TextDriver()
+	{
+		ui = new PersonUI();
+	}
+	
+	private void setScanner(Scanner sc)
+	{
+		this.sc = sc;
+	}
 	
 	public static void main(String[] args) {
 		int choice;
 		Scanner sc = new Scanner(System.in);
+		
 		TextDriver driver = new TextDriver();
+		driver.setScanner(sc);
+		
+		
 
 		do 
 			{
@@ -17,11 +36,39 @@ public class TextDriver {
 					driver.manageProfile();
 				else if(choice == 5)
 					driver.editProfile();
-				else
+				else if (choice == 0)
+					System.out.println("Goodbye");
+				else 
 					System.out.println("not valid try a different number");
 			}while( choice != 0);
+		sc.close();
 	}
-	public void login(){}
+	
+	public void login(){
+		System.out.println("Enter username");
+		String username = sc.nextLine();
+		System.out.println("Enter password");
+		String password = sc.nextLine();
+		System.out.println("Steal? true or false");
+		boolean steal = sc.nextBoolean();
+		System.out.println("logon result");
+		ui = ui.logOn(username, password, steal);
+		if (ui instanceof UserUI)
+		{
+			userUI = (UserUI) ui;
+			System.out.println("user");
+		}
+		else if (ui instanceof AdminUI)
+		{
+			adminUI = (AdminUI) ui;
+			System.out.println("admin");
+		}
+		else
+		{
+			System.out.println("Issssssuuuuue with login");
+		}
+
+	}
 	public void manageProfile(){}
 	public void manageSchools(){}
 	public void searchSchools(){}
@@ -32,7 +79,10 @@ public class TextDriver {
 	public void viewSchool(){}
 	public void viewSearchResults(){}
 	public void viewUser(){}
-	public void addUser(){}
+	public void addUser(){
+		
+		
+	}
 	public void addUniversity(){}
 	public void editUniversity(){}
 	public void viewSchoolAndRecommendations(){}
