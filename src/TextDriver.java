8e2bee11f6d3b280e82a31806fc0fdb7d0e07559
mmdaly <mmdaly@csbsju.edu>
2016-03-14 21:53:29 -0500

    Added a bunch of use cases

--- src/TextDriver.java
+++ src/TextDriver.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class TextDriver {
@@ -28,7 +29,8 @@ public class TextDriver {
 
 		do 
 			{
-				System.out.println("Pick a use case: /n 1: login /n 3: manage profile /n 5: editProfile /n 18: addPerson /n 0: Quit");
+				System.out.println("Pick a use case: \n 1: login \n 3: manage profile \n 5: editProfile \n 18: addPerson  ");
+				System.out.println(" 16: Manage People \n 0: Quit");
 				choice = sc.nextInt();
 				if (choice == 1)
 					driver.login();
@@ -40,6 +42,8 @@ public class TextDriver {
 					System.out.println("Goodbye");
 				else if (choice == 18)
 					driver.addPerson();
+				else if (choice == 16)
+					driver.managePeople();
 				else 
 					System.out.println("not valid try a different number");
 			}while( choice != 0);
@@ -78,15 +82,47 @@ public class TextDriver {
 
 	}
 	public void manageProfile(){}
-	public void manageSchools(){}
+	public void manageSchools(){
+		System.out.println("U10 Manage Schools");
+		if(this.adminUI ==null)
+			System.out.println("not logged in as admin");
+		else
+		{
+			List<School> schools = adminUI.getSchools();
+			for(School s : schools)
+			{
+				System.out.print("Name: " + s.getName() + " State: " + s.getState() + " Location: " + s.getLocation());
+				System.out.print(" Control: " + s.getControl() + " #of Students: " + s.getNumStudents() + " %Female: " + s.getPercentFemale());
+				System.out.print(" SATverbal: " + s.getSATVerb() + " SATmath:"+ s.getSATMath() + " Expenses: " + s.getExpenses());
+				System.out.print(" %with Financial Aid: " + s.getPercentFinancialAid() + " # of Applicants: " + s.getNumberOfApplicants());
+				System.out.print(" %admitted: " + s.getPercentAdmitted() + " %enrolled: " + s.getPercentEnrolled() );
+				System.out.print(" Academics scale: " + s.getAcademicsScale() + " Social Scale: " + s.getSocialScale() );
+				System.out.print(" Quality of Life: " + s.getQualityOfLifeScale());
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
 	public void searchSchools(){}
 	public void logout(){}
-	public void managePeople(){}
+	public void managePeople(){
+		if(this.adminUI ==null)
+			System.out.println("not logged in as admin");
+		else
+		{
+			List<Person> people = adminUI.getPeople();
+			for(Person p: people)
+			{
+				System.out.println("firstName: " + p.getFirstName() + " lastname: " + p.getLastName() + " username: " + 
+				 p.getUsername() + " password: " + p.getPassword() + " isAdmin: " + p.getIsAdmin() );
+			}
+		}
+	}
 	public void editProfile(){}
 	public void removeSchool(){}
 	public void viewSchool(){}
 	public void viewSearchResults(){}
-	public void viewPerson(){}
+	
 	public void addPerson(){
 		/*System.out.println("Enter firstname");
 		String firstName = sc.nextLine();
@@ -111,6 +147,24 @@ public class TextDriver {
 	public void addUniversity(){}
 	public void editUniversity(){}
 	public void viewSchoolAndRecommendations(){}
-	public void changeActiveStatusUser(){}
-	public void editPerson(){}
+	public void changeActiveStatusPerson(){
+		System.out.println("U20 Deactivate/Activate");
+		Person person = adminUI.getPersonByUsername("SuperAdmin");
+		adminUI.changeStatus(person);
+		System.out.println("Status changed on SuperAdmin");
+	}
+	public void updatePerson(){
+		System.out.println("U19 Edit Person");
+		Person person = adminUI.getPersonByUsername("SuperAdmin");
+		String firstName = person.getFirstName();
+		String lastName = person.getLastName() + "Edited";
+		String password = person.getPassword();
+		String type;
+		if (person.getIsAdmin())
+			 type = "a";
+		else
+			 type = "u";
+		adminUI.updatePerson(person, firstName, lastName, password, type);
+		System.out.println("Person SuperAdmin edited");
+	}
 }
