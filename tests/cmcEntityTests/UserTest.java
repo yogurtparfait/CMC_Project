40260a35f398ca3f3ae339cc26910bfbc3478ed1
Noah Froberg <ndfroberg@linclemensvm3.csbsju.edu>
2016-03-29 19:25:37 -0500

    added empty test classes

---
+++ tests/cmcEntityTests/UserTest.java
@@ -0,0 +1,22 @@
+package cmcEntityTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import entities.*;
+
+public class UserTest {
+
+	@Before
+	public void setUp() throws Exception {
+		User u = new User();
+	}
+	
+	@Test
+	public void test() {
+		//TODO
+	}
+
+}
