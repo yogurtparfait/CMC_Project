3b79da779d29cdf19d38e649287089fd1282c151
y1cao <y1cao@lin2.cs.csbsju.edu>
2016-03-30 21:08:12 -0500

    draft for persontest but i have no idea about Boolean part which are getIsAdmin and setIsAdmin. Be sure look at them-----Yang

--- tests/cmcEntityTests/PersonTest.java
+++ tests/cmcEntityTests/PersonTest.java
@@ -20,6 +20,34 @@ import static org.junit.Assert.*;
 			assertTrue("Person p is not null", !(p == null));
 		}
 
+		@Test
+		public void testGetters(){
+			Person p= new Person("first name","last name","username","password",isAdmin);
+			
+			assertTrue("first name works", p.getFirstName().equals("first name"));
+			assertTrue("last name works", p.getLastName().equals("last name"));
+			assertTrue("username works", p.getUsername().equals("username"));
+			assertTrue("password works", p.getPassword().equals("password"));	
+			//assertTrue("this is an admin",p.getIsAdmin())
+			
+		}
+		
+		@Test 
+		public void testSetters(){
+			Person p = new Person("first name","last name","username","password",isAdmin);
+			
+			p.setFirstName("fname");
+			p.setLastName("lname");
+			p.setUsername("username1");
+			p.setPassword("password1");
+			
+			assertTrue("first name works", p.getFirstName().equals("fname"));
+			assertTrue("last name works", p.getLastName().equals("lname"));
+			assertTrue("username works", p.getUsername().equals("username1"));
+			assertTrue("password works", p.getPassword().equals("password1"));
+			
+		}
+
 	}
 
 
