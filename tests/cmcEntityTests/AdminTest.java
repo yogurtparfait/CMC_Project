fa120909e36a42c34e5c6c69a47146b7c2aab151
y1cao <y1cao@lin2.cs.csbsju.edu>
2016-03-30 19:46:03 -0500

    draft for AdminTest.java -----Yang

--- tests/cmcEntityTests/AdminTest.java
+++ tests/cmcEntityTests/AdminTest.java
@@ -15,8 +15,19 @@ public class AdminTest {
 	}
 	
 	@Test
-	public void test() {
+	public void testSetters() {
 		//TODO
+		Admin a= new Admin("first name","last name","username","password");
+		
+		a.setFirstName("fname");
+		a.setLastName("lname");
+		a.setUsername("username1");
+		a.setPassword("password1");
+		
+		assertTrue("first name works", a.getFirstName().equals("fname"));
+		assertTrue("last name works", a.getLastName().equals("lname"));
+		assertTrue("username works", a.getUsername().equals("username1"));
+		assertTrue("password works", a.getPassword().equals("password1"));
 	}
 
 }
