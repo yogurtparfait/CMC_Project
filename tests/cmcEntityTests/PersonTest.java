507cac565f21a54e49d0afd0ea4f733dd5923154
mmdaly <mmdaly@csbsju.edu>
2016-03-30 22:30:15 -0500

    fixed persontest to not use nonexistant constructor

--- tests/cmcEntityTests/PersonTest.java
+++ tests/cmcEntityTests/PersonTest.java
@@ -8,45 +8,38 @@ import static org.junit.Assert.*;
 	import org.junit.Test;
 
 	public class PersonTest {
+		private Person p;
+		
 
 		@Before
 		public void setUp() throws Exception {
-			Person p = new Person();
+			p = new Person();
 		}
 		
 		@Test
 		public void testCreatePerson() {
-			Person p = new Person();
 			assertTrue("Person p is not null", !(p == null));
 		}
 
-		@Test
-		public void testGetters(){
-			Person p= new Person("first name","last name","username","password");
-			
-			assertTrue("first name works", p.getFirstName().equals("first name"));
-			assertTrue("last name works", p.getLastName().equals("last name"));
-			assertTrue("username works", p.getUsername().equals("username"));
-			assertTrue("password works", p.getPassword().equals("password"));	
-			//assertTrue("this is an admin",p.getIsAdmin())
-			
-		}
 		
 		@Test 
 		public void testSetters(){
-			Person p = new Person("first name","last name","username","password");
 			
 			p.setFirstName("fname");
 			p.setLastName("lname");
 			p.setUsername("username1");
 			p.setPassword("password1");
+			p.setIsAdmin(true);
 			
 			assertTrue("first name works", p.getFirstName().equals("fname"));
 			assertTrue("last name works", p.getLastName().equals("lname"));
 			assertTrue("username works", p.getUsername().equals("username1"));
 			assertTrue("password works", p.getPassword().equals("password1"));
+			assertTrue("isAdmin", (p.getIsAdmin() == true));
+			
 			
 		}
+		
 
 	}
 
